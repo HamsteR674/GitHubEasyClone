3ab98affeda85e5996490ee30624518c12dc49ee
HamsteR674 <fokir_mailrussia@list.ru>
2016-07-10 18:11:59 +0300

    Create filescan.hpp

---
+++ filescan.hpp
@@ -0,0 +1,15 @@
+using namespace std;
+
+struct Person { 
+string name; 
+string github; 
+string group; 
+string repository; 
+string catalogue;
+string grc;
+}; 
+
+Person group[500]; 
+int group_max_num=0; 
+
+int filescan();
